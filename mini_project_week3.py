67fae1c3aafe91cacd6920005f9d50176a4216df
Farida El Kafrawy <faridae@icloud.com>
2021-07-29 15:43:38 +0100

    view couriers and products functioning

--- mini_project_week3.py
+++ mini_project_week3.py
@@ -1,37 +1,47 @@
-from mini_project_week2 import courier_list
+from mini_project_week5 import view_couriers
+import csv
+import mysql.connector
 
-order_1 = {
-"customer_name": "John",
-"customer_address": "Unit 2, 12 Main Street, LONDON, WH1 2ER",
-"customer_phone": "0789887334",
-"courier": 1,
-"status": "preparing"
-}
-order_2 = {
-"customer_name": "Sam",
-"customer_address": "Unit 2, 24 Main Street, LONDON, WH1 2ER",
-"customer_phone": "0784447334",
-"courier": 2,
-"status": "preparing"
-}
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="Noorsis4",
+  database="mini_project"
+)
+mycursor = mydb.cursor()
+# order_1 = {
+# "customer_name": "John",
+# "customer_address": "Unit 2, 12 Main Street, LONDON, WH1 2ER",
+# "customer_phone": "0789887334",
+# "courier": 1,
+# "status": "Preparing"
+# }
+# order_2 = {
+# "customer_name": "Sam",
+# "customer_address": "Unit 2, 24 Main Street, LONDON, WH1 2ER",
+# "customer_phone": "0784447334",
+# "courier": 2,
+# "status": "Preparing"
+# }
 
-order_list = [order_1, order_2]
-print(order_list)
+with open("order_list.csv", "r") as f:
+    reader = csv.DictReader(f)
+    a = list(reader)
     
+order_list= a
+
 def add_order():
     customer_name = input("Enter customer name")
     customer_address = input("Enter customer address")
     customer_phone = input("Enter customer phone number")
-    with open('courier_list.txt', 'w') as f:
-            for i in range(len(courier_list)):
-                print(f"{i+1}. {courier_list[i].title()}")
+    view_couriers()
     courier_number= input("Which courier is delivering this order. Select number please.")
     new_order = {
         "customer_name": customer_name,
         "customer_address": customer_address,
         "customer_phone": customer_phone,
         "courier": courier_number,
-        "status": "preparing",
+        "status": "Preparing",
     }
     order_list.append(new_order)
     print(order_list)
@@ -41,13 +51,13 @@ def update_order():
         print(f"{i+1}. {order_list[i]}")
     update_choice = int(input("Which order do you want to update? Choose number"))
     update_property = int(input("""
-                                 What do you want to update?
-                                 1. Customer Name
-                                 2. Customer Address
-                                 3. Customer Phone Number
-                                 4. Courier
-                                 5. Order status
-                                 """))
+What do you want to update?
+1. Customer Name
+2. Customer Address
+3. Customer Phone Number
+4. Courier
+5. Order status
+    """))
     if update_property == 1:
         update_name = input("Enter new name")
         order_list[update_choice-1]["customer_name"] = update_name
@@ -55,15 +65,43 @@ def update_order():
             print(f"{i+1}. {order_list[i]}")
     elif update_property ==2:
         update_address= input("Enter new address")
-        order_list[update_choice-1]["customer_address"] = update_address
+        order_list[update_choice-1]["customer_address"] = "f{update_address}"
     elif update_property ==3:
         update_phone = input("Enter new phone number")
         order_list[update_choice-1]["customer_phone"] = update_phone
     elif update_property ==4:
-        update_courier= input("Enter new courier")
+        view_couriers()
+        update_courier= input("Which courier is delivering this order. Select number please.")
         order_list[update_choice-1]["courier"] = update_courier
     elif update_property ==5:
-        update_status = input("Enter new status")
-        order_list[update_choice-1]["status"] = update_status
-update_order()
-print(order_list)
+        update_status = int(input("""
+Enter new status
+1. Preparing
+2. With courier
+3. Delivered
+    """))
+        if update_status ==3:
+            order_list[update_choice-1]['status'] = "Delivered"
+        elif update_status ==2:
+            order_list[update_choice-1]['status'] = "With courier"
+        elif update_status ==1:
+            order_list[update_choice-1]['status'] = "Preparing"
+        print("Order delivery status updated")
+    
+def delete_order():
+    for i in range(len(order_list)):
+        print(f"{i+1}. {order_list[i]}")
+    order_to_delete = int(input("Which order do you wish to delete? (Select number)"))
+    order_list.pop(order_to_delete-1)
+    print("Order deleted")
+
+def order_save_and_exit():
+    with open('order_list.csv', 'w', encoding='utf8', newline='') as output_file:
+        fc = csv.DictWriter(output_file, 
+            fieldnames=order_list[0].keys())
+        fc.writeheader()
+        fc.writerows(order_list)
+        
+        
+# update_order()
+# order_save_and_exit()
